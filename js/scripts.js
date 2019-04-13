1f6d253f33ab4e0c3ab35908e029e54f46f6254e
Stewart ilondanga <stilondanga@gmail.com>
2019-04-13 11:24:59 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-/*$('.toolbar a').on('click', function(e) {
+$('.toolbar a').on('click', function(e) {
   e.preventDefault();
   document.execCommand($(this).data('role'), false);
 });
 
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
